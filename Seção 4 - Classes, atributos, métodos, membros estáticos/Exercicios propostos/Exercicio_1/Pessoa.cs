7980ec93b743ec6d32b05f9e1c252bfc67815931
Raphael Henrique Fernandes Rocha <rhfrocha@prodam.sp.gov.br>
2024-08-09 14:18:29 -0300

    Iniciando exercícios da 4ª seção

---
+++ Seção 4 - Classes, atributos, métodos, membros estáticos/Exercicios propostos/Exercicio_1/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1;
+internal class Pessoa
+{
+    public string Nome;
+    public int Idade;
+
+    
+    public Pessoa(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+
+
+    public override string ToString()
+    {
+        return $"\nNome: {Nome} \nIdade: {Idade}";
+    }
+
+}
---
+++ Seção 4 - Classes, atributos, métodos, membros estáticos/Exercicios propostos/Exercicio_1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Exercicio_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+            EXERCÍCIO 01: Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.
+        
+            Exemplo:
+            
+            Dados da primeira pessoa:
+            Nome: Maria
+            Idade: 17
+            Dados da segunda pessoa:
+            Nome: Joao
+            Idade: 16
+            Pessoa mais velha: Maria 
+        */
+
+
+        string nome;
+        int idade;
+
+        Console.Write("Digite o nome da primeira pessoa: ");
+        nome = Console.ReadLine();
+        Console.Write("Digite a idade da primeira pessoa: ");
+        idade = int.Parse(Console.ReadLine());
+
+        Pessoa PrimeiraPessoa = new Pessoa(nome, idade);
+
+        Console.Write("\n\nDigite o nome da segunda pessoa: ");
+        nome = Console.ReadLine();
+        Console.Write("Digite a idade da segunda pessoa: ");
+        idade = int.Parse(Console.ReadLine());
+
+        Pessoa SegundaPessoa = new Pessoa(nome, idade);
+
+        Console.WriteLine("\n\nDados da primeira pessoa: ");
+        Console.WriteLine($"{PrimeiraPessoa}");
+        Console.WriteLine("\nDados da segunda pessoa: ");
+        Console.WriteLine($"{SegundaPessoa}");
+
+        
+        if(PrimeiraPessoa.Idade > SegundaPessoa.Idade)
+        {
+            Console.WriteLine($"\nPessoa mais velha: {PrimeiraPessoa.Nome}");
+        }
+        else
+        {
+            Console.WriteLine($"\nPessoa mais velha: {SegundaPessoa.Nome}");
+        }
+    }
+}
