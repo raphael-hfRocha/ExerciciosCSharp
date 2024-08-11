f75c7247526b37816a0bf363f03a7ca0b93ed5a6
Raphael Henrique Fernandes Rocha <rhfrocha@prodam.sp.gov.br>
2024-08-11 15:30:37 -0300

    Exercício calculo de salário médio dos funcionários

---
+++ Seção 4 - Classes, atributos, métodos, membros estáticos/Exercicios propostos/Exercicio_2/SalarioMedioFuncionarios/Funcionario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioMedioFuncionarios;
+public class Funcionario
+{
+    public string Nome;
+    public double Salario;
+
+    public override string ToString()
+    {
+        return $"\nNome: {Nome} \nSalario: {Salario:F2}";
+    }
+
+
+    /*
+        private string _nome;
+        private double _salario;
+   
+
+        public string Nome { get => getNome(); set => setNome(value); }
+        public double Salario { get => getSalario(); set => setSalario(value); }
+
+        public Funcionario()
+        {
+        }
+
+        public Funcionario(string nome, double salario)
+        {
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public string getNome()
+        {
+            return _nome;
+        }
+        public void setNome(string value)
+        {
+            this._nome = value;
+        }
+        public double getSalario()
+        {
+            return _salario;
+        }
+        public void setSalario(double value)
+        {
+            this._salario = value;
+        }
+    */
+}
---
+++ Seção 4 - Classes, atributos, métodos, membros estáticos/Exercicios propostos/Exercicio_2/SalarioMedioFuncionarios/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace SalarioMedioFuncionarios;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+            EXERCÍCIO 02 - Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários. 
+                    
+            Exemplo:
+
+            Dados do primeiro funcionário:
+            Nome: Carlos Silva
+            Salário: 6300.00
+            Dados do segundo funcionário:
+            Nome: Ana Marques
+            Salário: 6700.00
+            Salário médio = 6500.00
+
+         */
+
+        string Nome, Salario;
+
+        Funcionario f1 = new Funcionario();
+        Funcionario f2 = new Funcionario();
+
+        Console.Write("Digite o nome do primeiro funcionário: ");
+        f1.Nome = Console.ReadLine();
+        Console.Write("Digite o salário do primeiro funcionário: ");
+        f1.Salario = double.Parse(Console.ReadLine());
+        Console.Write("\nDigite o nome do segundo funcionário: ");
+        f2.Nome = Console.ReadLine();
+        Console.Write("Digite o salário do segundo funcionário: ");
+        f2.Salario = double.Parse(Console.ReadLine());
+
+
+        double mediaSalario = (f1.Salario + f2.Salario) / 2;
+
+
+        Console.WriteLine("\nDados do primeiro funcionário:");
+        Console.WriteLine(f1);
+        Console.WriteLine("\nDados do segundo funcionário:");
+        Console.WriteLine(f2);
+        Console.WriteLine($"\nSalario médio: {mediaSalario:F2}");
+
+    }
+}
