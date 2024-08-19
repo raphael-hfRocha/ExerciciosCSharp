f01d9f9ba539356cd2627f6da2287435e137049a
Raphael Henrique Fernandes Rocha <rhfrocha@prodam.sp.gov.br>
2024-08-19 15:32:27 -0300

    Subindo o exercicio 4

---
+++ Seção 4 - Classes, atributos, métodos, membros estáticos/Exercicios propostos/Exercicio_4/RetanguloMedidas/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RetanguloMedidas;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Retangulo r = new Retangulo();
+
+        Console.Write("Digite a largura do retângulo: ");
+        r.Largura = double.Parse(Console.ReadLine());
+        Console.Write("Digite a altura do retângulo: ");
+        r.Altura = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nDados: \n");
+        Console.WriteLine(r);
+    }
+}
---
+++ Seção 4 - Classes, atributos, métodos, membros estáticos/Exercicios propostos/Exercicio_4/RetanguloMedidas/Retangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetanguloMedidas;
+public class Retangulo
+{
+    public double Largura;
+    public double Altura;
+
+    public double Perimetro()
+    {
+        return (Largura * 2) + (Altura * 2);
+    }
+
+    public double Area()
+    {
+        return Largura * Altura;
+    }
+
+    public double Diagonal()
+    {
+        return Math.Sqrt((Math.Pow(Largura, 2) + Math.Pow(Altura, 2)));
+    }
+     
+    public override string ToString()
+    {
+        return $"Perimetro: {Perimetro():F2} \nArea: {Area():F2} \nDiagonal: {Diagonal():F2}";
+    }
+}
