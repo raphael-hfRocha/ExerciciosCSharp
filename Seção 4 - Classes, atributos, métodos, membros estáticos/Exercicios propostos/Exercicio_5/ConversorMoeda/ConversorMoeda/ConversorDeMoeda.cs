cfc65a00d341c2f40fc76c31111f0e7b3c35a3f1
Raphael Henrique Fernandes Rocha <rhfrocha@prodam.sp.gov.br>
2024-08-25 19:04:06 -0300

    Exercício 5: Conversor de moeda com a utilização de membros estáticos

---
+++ Seção 4 - Classes, atributos, métodos, membros estáticos/Exercicios propostos/Exercicio_5/ConversorMoeda/ConversorMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorMoeda;
+public class ConversorDeMoeda
+{
+    public static double IOF = 6;
+
+    public static double CalculoCompraDolar(double cotacao, double precoDolar)
+    {
+        double calculoIOF = (cotacao * precoDolar) * (IOF / 100);
+
+        return (cotacao * precoDolar) + calculoIOF;
+    }
+}
---
+++ Seção 4 - Classes, atributos, métodos, membros estáticos/Exercicios propostos/Exercicio_5/ConversorMoeda/ConversorMoeda/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConversorMoeda;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+            Exercício 5 - Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado 
+            por uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda
+            que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. Criar uma classe ConversorDeMoeda
+            para ser responsável pelos cálculos.
+        */
+
+        double cotacao, precoDolar;
+
+        Console.Write("Digite o valor da cotação em dolar: ");
+        cotacao = double.Parse(Console.ReadLine());
+        Console.Write("Digite o valor do dolar que deseja comprar: ");
+        precoDolar = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nCotação do dolar: {cotacao:F2}");
+        Console.WriteLine($"Valor do dolar que deseja comprar: {precoDolar:F2}");
+        Console.WriteLine($"Valor a ser pago em reais: {ConversorDeMoeda.CalculoCompraDolar(cotacao, precoDolar):F2}");
+
+
+    }
+}
