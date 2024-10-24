91502691f1bdeea7070c8885f702f0cfe708f4ab
Raphael Henrique Fernandes Rocha <rhfrocha@prodam.sp.gov.br>
2024-10-24 14:52:19 -0300

    Climbing the StringBuilder exercise

---
+++ Seção 8 - Enumerações e composição/Exercícios propostos/Exercicio_2/ExercicioStringBuilder/Entities/Models/Post.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioStringBuilder.Entities.Models;
+public class Post
+{
+    private DateTime Moment = DateTime.Now;
+    private string _title;
+    private string _content;
+    private int _like;
+    public List<Comment> Comments = new List<Comment>();
+
+    public string Title { get => getTitle(); set => setTitle(value); }
+    public string Content { get => getContent(); set => setContent(value); }
+    public int Like { get => getLike(); set => setLike(value); }
+
+    public Post(string title, string content, int like)
+    {
+        this.Title = title;
+        this.Content = content;
+        this.Like = like;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        Comments.Add(comment);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine(Title);
+        sb.Append(Like);
+        sb.Append(" - ");
+        sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+        sb.AppendLine(Content);
+        sb.AppendLine("\nComments: \n");
+        foreach(Comment c in Comments)
+        {
+            sb.AppendLine(c.Text);
+        }
+
+        return sb.ToString();
+    }
+
+    private string getTitle()
+    {
+        return _title;
+    }
+    private void setTitle(string valor)
+    {
+        this._title = valor;
+    }
+    private string getContent()
+    {
+        return _content;
+    }
+    private void setContent(string valor)
+    {
+        this._content = valor;
+    }
+    private int getLike()
+    {
+        return _like;
+    }
+    private void setLike(int valor)
+    {
+        this._like = valor;
+    }
+}
---
+++ Seção 8 - Enumerações e composição/Exercícios propostos/Exercicio_2/ExercicioStringBuilder/Program.cs
@@ -0,0 +1,37 @@
+﻿using ExercicioStringBuilder.Entities.Models;
+
+namespace ExercicioStringBuilder;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Post p;
+        Comment c = new Comment();
+        string title, content, comment;
+        int like, qtde;
+
+        Console.Write("Digite um título para a publicação: ");
+        title = Console.ReadLine();
+        Console.Write("Coloque o conteúdo da publicação: ");
+        content = Console.ReadLine();
+        Console.Write("Coloque a quantidade de likes: ");
+        like = int.Parse(Console.ReadLine());
+        p = new Post(title, content, like);
+        Console.Write("Digite a quantidade de comentários: ");
+        qtde = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n");
+        for(int i = 1; i <= qtde; i++)
+        {
+            Console.Write($"\nDigite o {i}º comentario: ");
+            c.Text = Console.ReadLine();
+            p.AddComment(c);
+        }
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine(p);
+
+    }
+}
